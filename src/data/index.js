84a6ddf5233e33dc5b0cc0efc74fd063f6c4d879
Thomas A. Reinert <thomas@tarcgn.de>
2018-03-14 12:43:44 +0100

    Add / delete Meta-Data

--- src/data/index.js
+++ src/data/index.js
@@ -5,7 +5,8 @@ module.exports = {
   "author":"Thomas A. Reinert",
   "title_page":"WordPress Developer Checklist | TAR MediaDesign",
   "site_name": "WordPress Developer Checklist | TAR MediaDesign",
-  "locale":"de",
-  "twitter_site":"https://www.tarcgn.de/wordpress-checklist",
-  "twitter_creator":"@ThomasAReinert"
+  "locale":"en",
+  "github_repo":"https://github.com/Thomas-A-Reinert/WordPress-Developer-Checklist",
+  // "twitter_site":"https://www.tarcgn.de/wordpress-checklist",
+  // "twitter_creator":"@ThomasAReinert"
 };
