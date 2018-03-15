caba76f4498a91e8b18c9a2f20a51872b997609b
Thomas A. Reinert <thomas@tarcgn.de>
2018-03-15 13:41:29 +0100

    Injecting variable for lang-attribute from index.js

--- src/data/index.js
+++ src/data/index.js
@@ -1,12 +1,14 @@
+var module;
 module.exports = {
-  "url_website":"https://www.tarcgn.de/wordpress-checklist",
-  "url_base": "/",
-  "description":"A (hopefully) complete Checklist for WordPress developers.",
-  "author":"Thomas A. Reinert",
-  "title_page":"WordPress-Developer-Checklist | TAR MediaDesign",
-  "site_name": "WordPress-Developer-Checklist | TAR MediaDesign",
-  "locale":"en",
-  "github_repo":"https://github.com/Thomas-A-Reinert/WordPress-Developer-Checklist",
-  // "twitter_site":"https://www.tarcgn.de/wordpress-checklist",
-  // "twitter_creator":"@ThomasAReinert"
+  url_website: "https://wpchecklist.tarthemes.com",
+  url_base: "/",
+  description: "A (hopefully) complete Checklist for WordPress developers.",
+  author: "Thomas A. Reinert",
+  title_page: "WordPress-Developer-Checklist | TAR MediaDesign",
+  site_name: "WordPress-Developer-Checklist | TAR MediaDesign",
+  locale: "en",
+  github_repo:
+    "https://github.com/Thomas-A-Reinert/WordPress-Developer-Checklist",
+  twitter_site: "https://www.tarcgn.de/wordpress-checklist",
+  twitter_creator: "@ThomasAReinert"
 };
